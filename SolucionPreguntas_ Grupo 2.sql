46d8b5c9f69f27ccf192756f59003af6ab93b143
IvanVelasquezQ <156323750+IvanVelasquezQ@users.noreply.github.com>
2024-02-01 12:12:58 -0500

    SolucionPreguntas_ Grupo 2.sql

--- SolucionPreguntas_ Grupo 2.sql
+++ SolucionPreguntas_ Grupo 2.sql
@@ -1,5 +1,5 @@
 --Preguntas de la base de datos grupo 2
---1.	øCuantos clientes han realizado su registro elÈctrico por Distritos en el aÒo 2023?
+--1.	¬øCuantos clientes han realizado su registro el√©ctrico por Distritos en el a√±o 2023?
 		select grupo2.distrito.Nomb_Distrito, count(*) as 'Cantidad'
 		from grupo2.RegistrosElectricos
 		INNER JOIN grupo2.Distrito
@@ -8,14 +8,31 @@
 		group by grupo2.distrito.Nomb_distrito
 		order by 'Cantidad' desc
 
---2.	Cu·l es la variciÛn de registros realizados del 2022 al 2023?
-		select count(*) from grupo2.RegistrosElectricos
-		where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2022
+--2.	Cu√°l es la variaci√≥n de registros realizados del 2023 al 2022?
+		--Soluci√≥n 1
+		declare @reg2022 float
+		set @reg2022 = (select count(*) from grupo2.RegistrosElectricos
+		where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2022)
 		
-		select count(*) from grupo2.RegistrosElectricos
-		where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2023
+		declare @reg2023 float
+		set @reg2023= (select count(*) from grupo2.RegistrosElectricos
+		where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2023)
+		Print @reg2023/@reg2022-1
+		
+		--Soluci√≥n 2
+		select (convert(decimal(10,2),
+			(select count(*) from grupo2.RegistrosElectricos 
+			where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2023)) 
+			- 		
+			convert(decimal(10,2),
+			(select count(*) from grupo2.RegistrosElectricos
+			where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2022)))
+			/
+			convert(decimal(10,2),
+			(select count(*) from grupo2.RegistrosElectricos
+			where YEAR(grupo2.RegistrosElectricos.Fecha_Puesta_Servicio)=2022))
 
---3.	QuÈ modelo de controlador es el m·s instalado por distrito del 2023?
+--3.	Qu√© modelo de controlador es el m√°s instalado por distrito del 2023?
 		select Dist.Nomb_Distrito,Cont.Modelo_Controlador, count(*) as 'Cantidad'
 		from grupo2.RegistrosElectricos RegElec
 		INNER JOIN grupo2.Distrito Dist
@@ -25,14 +42,14 @@
 		group by Cont.Modelo_Controlador,Dist.Nomb_Distrito
 		order by 'Cantidad' desc
 
---4.	Cual es la cantidad de registros elÈctricos que se encuentran operativos 2023?
+--4.	Cual es la cantidad de registros el√©ctricos que se encuentran operativos 2023?
 		select count(*) as 'Cantidad Operativos'
 		from grupo2.RegistrosElectricos RegElec
 		INNER JOIN grupo2.Detalle_Estado Detalle
 		on RegElec.id_Detalle_Estado=Detalle.id_Detalle_Estado
 		where Detalle.id_Detalle_Estado=1
 		
---5.	Cu·l es el tipo de tensiÛn de los suministros elÈctricos que m·s solicitan los clientes (M, T) durante los ˙ltimos 3 meses?
+--5.	Cu√°l es el tipo de tensi√≥n de los suministros el√©ctricos que m√°s solicitan los clientes (M, T) durante los √∫ltimos 3 meses?
 		select Sumi.Descripcion_Tension, count(*) as 'Cantidad'
 		from grupo2.Suministro Sumi
 		inner join grupo2.RegistrosElectricos RegElec
@@ -42,7 +59,7 @@
 		MONTH(RegElec.Fecha_Puesta_Servicio)=5
 		group by Sumi.Descripcion_Tension
 
---6.	Que marca de baterÌa son los m·s instalados en el ˙ltimo trimestre?
+--6.	Que marca de bater√≠a son los m√°s instalados en el √∫ltimo trimestre?
 		select Bate.Marca_Bateria, count(*) as 'Cantidad'
 		from grupo2.RegistrosElectricos RegElec
 		INNER JOIN grupo2.Bateria Bate
@@ -53,16 +70,16 @@
 		group by Bate.Marca_Bateria
 		order by 'Cantidad' desc
 
---7.	Cu·l es la cantidad de suministros instalados de tipo trif·sico en el aÒo 2022?
+--7.	Cu√°l es la cantidad de suministros instalados de tipo trif√°sico en el a√±o 2022?
 		select Sumi.Descripcion_Tension, count(*) as 'Cantidad'
 		from grupo2.Suministro Sumi
 		inner join grupo2.RegistrosElectricos RegElec
 		on sumi.id_Suministro=RegElec.id_Suministro
 		where YEAR(RegElec.Fecha_Puesta_Servicio)=2022 and
-		Sumi.Descripcion_Tension='Trif·sico'
+		Sumi.Descripcion_Tension='Trif√°sico'
 		group by Sumi.Descripcion_Tension
 
---8.	Cu·l es la marca de batrÌa m·s instalados en los suministros del 2023?
+--8.	Cu√°l es la marca de batr√≠a m√°s instalados en los suministros del 2023?
 		select Bate.Marca_Bateria, count(*) as 'Cantidad'
 		from grupo2.RegistrosElectricos RegElec
 		INNER JOIN grupo2.Bateria Bate
@@ -71,7 +88,7 @@
 		group by Bate.Marca_Bateria
 		order by 'Cantidad' desc
 
---9.	Que Distritos solicitan m·s suministros elÈctricos de tipo monof·sico en el 2022 y 2023?
+--9.	Que Distritos solicitan m√°s suministros el√©ctricos de tipo monof√°sico en el 2022 y 2023?
 		select Dist.Nomb_Distrito, count(*) as 'Cantidad'
 		from grupo2.Suministro Sumi
 		inner join grupo2.RegistrosElectricos RegElec
@@ -80,11 +97,11 @@
 		on RegElec.id_distrito=Dist.id_distrito
 		where (YEAR(RegElec.Fecha_Puesta_Servicio)=2022 or
 		YEAR(RegElec.Fecha_Puesta_Servicio)=2023) and
-		Sumi.Descripcion_Tension='Monof·sico'
+		Sumi.Descripcion_Tension='Monof√°sico'
 		group by Dist.Nomb_Distrito
 
---10.	Cu·ntos suministros tienen baterÌa con falla en el aÒo 2023?
-		select count(*) as 'Cantidad Falla BaterÌa'
+--10.	Cu√°ntos suministros tienen bater√≠a con falla en el a√±o 2023?
+		select count(*) as 'Cantidad Falla Bater√≠a'
 		from grupo2.RegistrosElectricos RegElec
 		INNER JOIN grupo2.Detalle_Estado Detalle
 		on RegElec.id_Detalle_Estado=Detalle.id_Detalle_Estado
